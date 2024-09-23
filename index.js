6dc08e41ce1b4bbfdbc1e040cb5eaf8172f30ebc
fahma20 <fahmaxo@gmail.com>
2024-09-23 13:04:16 +0300

    review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code in this file!
+const currentUser= 'Fahmo Abdisalan';
+console.log (currentUser);
+const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`
+console.log (welcomeMessage);
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+console.log (excitedWelcomeMessage);
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`
+console.log (shortGreeting)
